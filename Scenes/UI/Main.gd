e94173a11d060a544b0ae6b6dcd50548faed56d7
NeonNito <151113641+NeonNito@users.noreply.github.com>
2025-08-25 22:34:36 -0500

    Full Unification

--- Scenes/UI/Main.gd
+++ Scenes/UI/Main.gd
@@ -1,26 +1,18 @@
 extends Control
 
-@onready var buildings_list: RichTextLabel = $PanelContainer2/Panel/VBoxContainer/MarginContainer3/Buildings_List
-@onready var prod_total: RichTextLabel = $PanelContainer2/Panel/VBoxContainer/MarginContainer4/PROD_TOTAL
-@onready var capacity_bar: ProgressBar = $PanelContainer/VBoxContainer/Panel2/CAPACITY_BAR
-@onready var bio_mass_total: RichTextLabel = $PanelContainer/VBoxContainer/Panel2/VBoxContainer/BioMassTotal
-@onready var bms_btn: Button = $BMS_BTN
-@onready var rich_text_label: RichTextLabel = $RichTextLabel
-@onready var v_box_container: VBoxContainer = $PanelContainer/VBoxContainer/Panel/MarginContainer/VBoxContainer
-
 var buttonAdd = preload("res://button_template.tscn")
 var instancebuttonAdd = buttonAdd.instantiate()
 
-func _ready() -> void:
-	buildings_list.text = str("")
+#func _ready() -> void:
+
 func _process(delta: float) -> void:
-	bio_mass_total.text = "Biomass: " + str(int(GB.Biomass)) + " / " + str(GB.BiomassMAX)
-	prod_total.text = "Biomass Production: \n" + str(GB.ProdTotal) + " Per Second"
-	capacity_bar.value = GB.Biomass
-	rich_text_label.text = "Supply: " + str(GB.SupplyTotal) + " / " + str(GB.SupplyMax)
-	capacity_bar.value = GB.Biomass
-	capacity_bar.max_value = GB.BiomassMAX
-	Check_Unlocks()
+#	bio_mass_total.text = "Biomass: " + str(int(GB.Biomass)) + " / " + str(GB.BiomassMAX)
+#	prod_total.text = "Biomass Production: \n" + str(GB.ProdTotal) + " Per Second"
+#	capacity_bar.value = GB.Biomass
+#	rich_text_label.text = "Supply: " + str(GB.SupplyTotal) + " / " + str(GB.SupplyMax)
+#	capacity_bar.value = GB.Biomass
+#	capacity_bar.max_value = GB.BiomassMAX
+#	Check_Unlocks()
 	Production((GB.ProdTotal * delta))
 	display_buildings()
 
@@ -33,7 +25,6 @@ func Production(amount):
 func IncreaseProd(amount):
 	GB.ProdTotal += amount 
 	
-		
 func Add_item_Dict(Item, amount):
 	if Item in GB.buildings_list_DICT:
 		GB.buildings_list_DICT[Item] += amount
@@ -41,19 +32,19 @@ func Add_item_Dict(Item, amount):
 		GB.buildings_list_DICT[Item] = amount
 
 var test:bool = false
-
-func Check_Unlocks():
-	if GB.SupplyTotal >= 10:
-		var gross = 1
-	for item_name in GB.buildings_list_DICT:
-		if GB.buildings_list_DICT["Drones"] >= 5 and test == false:
-			test = true
+#
+#func Check_Unlocks():
+	#if GB.SupplyTotal >= 10:
+		#var gross = 1
+	#for item_name in GB.buildings_list_DICT:
+		#if GB.buildings_list_DICT["Drones"] >= 5 and test == false:
+			#test = true
 			
 func display_buildings():
-	buildings_list.text = str("")
+#	buildings_list.text = str("")
 	for item_name in GB.buildings_list_DICT:
 		var item_count = GB.buildings_list_DICT[item_name]
-		buildings_list.text += str(item_name) + ": " + str(item_count) + "\n"
+		#buildings_list.text += str(item_name) + ": " + str(item_count) + "\n"
 		
 func _on_bms_btn_pressed() -> void:
 	CreateBiomass(GB.ClickAmount)
